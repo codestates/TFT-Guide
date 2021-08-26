461512a6754ffebea1bcc313c9f47fb314d00a42
hyeon3051 <hyeon3051@gmail.com>
2021-08-26 13:43:28 +0900

    Feat: 데이터베이 스 스키마 작성

--- server/index.js
+++ server/index.js
@@ -4,6 +4,13 @@ const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 const DEV_CLIENT = process.env.DEV_CLIENT;
 
+const models = require("./models/index.js")
+
+const loginController = require("./controller/user/loginController")
+const registerContler = require("./controller/user/registerController")
+const infoController = require("./controller/user/infoController")
+
+
 app.use(express.json());
 
 app.use(
@@ -14,9 +21,26 @@ app.use(
     })
   );
 
+/**
+ * 유저 컨트롤러 
+ */
+app.post('/user/callback' ,loginController )
+
+app.post('/user', registerContler)
+
+app.get("/user/info", infoController)
+
+/**
+ * 서버 열기 
+ */
+
+ models.sequelize.sync().then(()=>{
+  console.log("DB 연결 성공")
+}) 
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
 
 module.exports = app;
